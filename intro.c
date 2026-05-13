ce67199315c50946e7d4ff0a54d9594e48b959de
Madhav Takkar <madhavtakkar9@gmail.com>
2026-05-13 11:08:20 +0530

    adding files

---
+++ intro.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+    // printing (this is a single line comment)
+
+    /* multiline
+    comment 
+    */
+
+    printf("hello,world!\n");
+    return 0;
+    // variables
+    // name of variable - is known as "identifier"
+    int var;                            //variable declaration
+    var = 1;                            //variable initialization
+    // int - integer datatype
+    
+    char var2 = 'a'; // in char datatype only one character is allowded
+    char var3;
+    var3 = 'b';
+    // day1 ends here 
+
+    
+    
+} 
