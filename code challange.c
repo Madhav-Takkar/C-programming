a9bee539db679c2f72866d13b590e2d90d8430a5
Madhav-Takkar <madhavtakkar9@gmail.com>
2026-05-19 15:14:05 +0530

    ques 1 done

---
+++ code challange.c
@@ -0,0 +1,15 @@
+//#include <stdio.h>
+//
+//int main() {
+//  // 1) Print "Hello World!" with a new line
+//  // 2) Print "I am learning C."
+//
+//
+//  printf("Hello World!\n");
+//  printf("I am learning C.");
+//  
+//  return 0;
+//}
+//
+
+
